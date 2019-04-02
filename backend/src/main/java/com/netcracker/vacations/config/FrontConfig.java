0ca7cc08b4ddf1f759d1b19e4dca330fe33ba47a
StudentKA <annakapranova77@gmail.com>
2019-04-02 16:46:26 +0300

    now return index.html (for absent mappings in controller)

--- backend/src/main/java/com/netcracker/vacations/config/FrontConfig.java
+++ backend/src/main/java/com/netcracker/vacations/config/FrontConfig.java
@@ -1,35 +1,22 @@
 package com.netcracker.vacations.config;
 
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-/*I'm trying to make SPA work with Spring properly*/
-
-//@Configuration
-//@EnableWebMvc
+@Configuration
 public class FrontConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:\\w+}")
-                .setViewName("forward:/resources/public/index.html");
-        registry.addViewController("/**/{spring:\\w+}")
-                .setViewName("forward:/resources/public/index.html");
-        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-                .setViewName("forward:/resources/public/index.html");
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryWebServerFactoryCustomizer() {
+        return container -> {
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+        };
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/{spring:\\w+}")
-                .addResourceLocations("forward:/resources/public/");
-        registry.addResourceHandler("/**/{spring:\\w+}")
-                .addResourceLocations("forward:/resources/public/");
-        registry.addResourceHandler("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-                .addResourceLocations("forward:/resources/public/");
-    }
 }
--- backend/src/main/java/com/netcracker/vacations/controller/TempController.java
+++ backend/src/main/java/com/netcracker/vacations/controller/TempController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping()
 public class TempController {
 
-    @GetMapping("/calendar")
+    @GetMapping("api/calendar")
     public ResponseEntity getCalendar() {
         return ResponseEntity.ok("calendar!");
     }
