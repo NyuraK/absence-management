7bf3dcc9f1005375270cf390abccd67eb9d6c206
StudentKA <annakapranova77@gmail.com>
2019-04-06 22:10:59 +0300

    resolve merge coflitct

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -29,7 +29,7 @@ const router = new VueRouter({
             meta: {rule: 'isLoggedUser'}
         },
         {
-            path: "*",
+            path: "/error",
             component: NotFound,
             meta: {rule: '*'}
         }
