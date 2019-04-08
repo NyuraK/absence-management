439bd3235ddd09b628b094c5bb532034e9b72ca5
DenisMitskovski <denism1111198@gmail.com>
2019-04-08 16:59:41 +0300

    Фикс на merge.

--- backend/src/main/java/com/netcracker/vacations/domain/TeamsEntity.java
+++ backend/src/main/java/com/netcracker/vacations/domain/TeamsEntity.java
@@ -1,55 +1,39 @@
 package com.netcracker.vacations.domain;
 
 
-import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 
 @Entity
-@Table(name="teams_ent")
+@Table(name="teams")
 public class TeamsEntity {
     @Id
     @GeneratedValue(generator = "increment")
-    @GenericGenerator(name= "increment", strategy= "increment")
-
 
     @Column(name = "teams_id")
         private Integer teamsId;
 
-    @Column(name = "employees_on_red", nullable=false)
-        private Integer red;
+    @Column(name = "abscense_quota", nullable=false)
+        private Integer quota;
 
-    @Column(name = "employees_on_green", nullable=false)
-        private Integer green;
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
 
     @JoinColumn(name = "managers_id")
-        private UsersEntity managersId;
+        private UserEntity managersId;
 
     @Column(name = "name", nullable = false, unique = true)
         private String name;
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
 
     @JoinColumn(name = "departments_id")
-        private DepartmentsEntity departmentsId;
+        private DepartmentEntity departmentsId;
 
 
     public TeamsEntity() {
     }
 
-    public TeamsEntity(Integer red, Integer green, UsersEntity managersId, DepartmentsEntity departmentsId, String name) {
-        this.red = 5;
-        this.green = 2;
-        this.managersId = managersId;
-        this.departmentsId = departmentsId;
-        this.name = name;
-    }
-
-    public TeamsEntity(Integer red, Integer green, UsersEntity managersId, String name) {
-        this.red = 5;
-        this.green = 2;
+    public TeamsEntity(Integer quota, UserEntity managersId, String name) {
+        this.quota = quota;
         this.managersId = managersId;
         this.name = name;
     }
@@ -62,35 +46,27 @@ public class TeamsEntity {
         this.teamsId = teamsId;
     }
 
-    public Integer getRed() {
-        return red;
+    public Integer getQuota() {
+        return quota;
     }
 
-    public void setRed(Integer red) {
-        this.red = red;
+    public void setQuota(Integer quota) {
+        this.quota = quota;
     }
 
-    public Integer getGreen() {
-        return green;
-    }
-
-    public void setGreen(Integer green) {
-        this.green = green;
-    }
-
-    public UsersEntity getManagersId() {
+    public UserEntity getManagersId() {
         return managersId;
     }
 
-    public void setManagersId(UsersEntity managersId) {
+    public void setManagersId(UserEntity managersId) {
         this.managersId = managersId;
     }
 
-    public DepartmentsEntity getDepartmentsId() {
+    public DepartmentEntity getDepartmentsId() {
         return departmentsId;
     }
 
-    public void setDepartmentsId(DepartmentsEntity departmentsId) {
+    public void setDepartmentsId(DepartmentEntity departmentsId) {
         this.departmentsId = departmentsId;
     }
 
