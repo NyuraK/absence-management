87f9ce3b69fc8da9902f54710d1e1e7df52dbb50
StudentKA <annakapranova77@gmail.com>
2019-04-11 15:26:44 +0300

    clean up in controller methods

--- backend/src/main/java/com/netcracker/vacations/controller/RequestController.java
+++ backend/src/main/java/com/netcracker/vacations/controller/RequestController.java
@@ -4,7 +4,6 @@ import com.netcracker.vacations.dto.RequestDTO;
 import com.netcracker.vacations.dto.RestRequestDTO;
 import com.netcracker.vacations.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,26 +17,6 @@ public class RequestController {
     @Autowired
     public RequestController(RequestService service) {
         this.service = service;
-    }
-
-    @GetMapping("/calendar")
-    public ResponseEntity getCalendar() {
-        return ResponseEntity.ok("calendar!");
-    }
-
-    @GetMapping("/admin/home")
-    public ResponseEntity getPersonalView() {
-        return ResponseEntity.ok("Welcome, Admin!!");
-    }
-
-    @GetMapping("/user/home")
-    public ResponseEntity getPersonalViewUser() {
-        return ResponseEntity.ok("Welcome, User!");
-    }
-
-    @GetMapping("/manager/home")
-    public ResponseEntity submitRequest() {
-        return ResponseEntity.ok("Welcome, Manager!");
     }
 
     @PostMapping("/request/add")
