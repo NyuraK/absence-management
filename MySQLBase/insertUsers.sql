444a5b27868ffa2e6504df25542007f771674a05
StudentKA <annakapranova77@gmail.com>
2019-05-05 21:53:13 +0300

    sql scripts changed a bit

--- MySQLBase/insertUsers.sql
+++ MySQLBase/insertUsers.sql
@@ -1,3 +1,4 @@
+INSERT INTO `autentification`.`users` (`users_id`, `family_name`, `hire_date`, `login`, `name`, `password`, `rest_days`, `role`) VALUES ('1', 'Kapranova', '2019-02-18', 'Anna', 'Anna', '$2a$10$XvPn3H6rTgGjGih/IYPPK.ZNG9VxvDDVJ3LQGkk6qrXJ4.II/0/4a', '60', 'ADMIN');
 INSERT INTO `autentification`.`users` (`users_id`, `hire_date`, `login`, `name`, `password`, `rest_days`, `role`) VALUES ('4', '2019-04-21', 'Artem', 'Artem', '$2a$10$XvPn3H6rTgGjGih/IYPPK.ZNG9VxvDDVJ3LQGkk6qrXJ4.II/0/4a', '30', 'EMPLOYEE');
 INSERT INTO `autentification`.`users` (`users_id`, `hire_date`, `login`, `name`, `password`, `rest_days`, `role`) VALUES ('5', '2019-02-22', 'Denis', 'Denis', '$2a$10$XvPn3H6rTgGjGih/IYPPK.ZNG9VxvDDVJ3LQGkk6qrXJ4.II/0/4a', '30', 'MANAGER');
 #пароль у всех дефолтный qwerty
